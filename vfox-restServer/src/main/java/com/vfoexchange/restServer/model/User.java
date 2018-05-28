dd0362a34acdf72ae71eee215ea13653b91c288f
Yatin-Arora <yatin.arora@tekmindz.com>
2018-05-28 18:16:17 +0530

    [Day Commit]: VFOX1.0 POC - PR review update

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/model/User.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/model/User.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-
 public class User implements UserDetails {
 
     private int id;
