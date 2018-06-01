e19bb33119a884b7c56f31d04707f0ec244a115c
pushpendranirwan <pushpendra.kumar@tekmindz.com>
2018-06-01 17:34:54 +0530

    [day commit] : Update Custom exception handler

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/controllerAdvice/CustomizedResponseEntityExceptionHandler.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/controllerAdvice/CustomizedResponseEntityExceptionHandler.java
@@ -3,6 +3,8 @@ package com.vfoexchange.restServer.controllerAdvice;
 
 import com.vfoexchange.restServer.exceptions.UserNotFoundException;
 import com.vfoexchange.restServer.model.ErrorDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,8 +18,11 @@ import java.util.Date;
 @RestController
 public class CustomizedResponseEntityExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomizedResponseEntityExceptionHandler.class);
+
     @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<ErrorDetails> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        LOGGER.debug("handling 404 error on a user entry");
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
