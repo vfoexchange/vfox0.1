b7f559406b4f0b412e99ca43b1056cff6317cf3d
pushpendranirwan <pushpendra.kumar@tekmindz.com>
2018-06-04 17:48:47 +0530

    [Day Commit] : Add Custom Exception Comments

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/exceptions/UserRoleNotFoundException.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/exceptions/UserRoleNotFoundException.java
@@ -1,5 +1,8 @@
 package com.vfoexchange.restServer.exceptions;
 
+/*
+Add custom exception class for  for user role not found
+ */
 public class UserRoleNotFoundException extends RuntimeException{
 
     public UserRoleNotFoundException(String role) {
