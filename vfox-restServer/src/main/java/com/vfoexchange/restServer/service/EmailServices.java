c6f0de3acf4226c0361989928adc9ce72190d7ff
pushpendranirwan <pushpendra.kumar@tekmindz.com>
2018-06-13 16:12:27 +0530

    [Day Commit] : implements AWS SES Mail verification

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/service/EmailServices.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/service/EmailServices.java
@@ -1,8 +1,12 @@
 package com.vfoexchange.restServer.service;
 
-import com.vfoexchange.restServer.model.Mail;
-
+/*
+Send an email through Amazon SES using the AWS SDK for Java
+ */
 public interface EmailServices {
 
-    public void sendMail(Mail mail);
+    /*
+    Method for send email
+     */
+    public void sendMail(String toEmailId) throws Exception;
 }
