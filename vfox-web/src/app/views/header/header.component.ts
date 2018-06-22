ddd026bfce5de682782cfad8a3ea880a63eaa8d5
Pradeep kumar <pradeep.kumar@tekmindz.com>
2018-06-22 17:16:04 +0530

    [Day Commit] vfox-web - Modified header for logout user

--- vfox-web/src/app/views/header/header.component.ts
+++ vfox-web/src/app/views/header/header.component.ts
@@ -3,20 +3,30 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { HomeService } from '../../services/home.service';
 import { Response, Http, Headers } from '@angular/http';
 import { UtilService } from "../../common-services/util-services";
+import { Authentication } from "../../common-services/authentication";
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
-    moduleId: module.id,
-    selector: 'header-nav',
+  moduleId: module.id,
+  selector: 'header-nav',
   templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
   currentUser: any;
 
-  constructor(private utilService: UtilService) {
+  constructor(private utilService: UtilService,private router: Router, private auth: Authentication,) {
     //Get current user
     this.currentUser = this.utilService.getData('loginDataDetail');
 
   }
+
+  logout() {
+    this.auth.logout().subscribe(
+      () => {
+        this.router.navigate(['/']);
+      }
+    );
+  }
+
 
 }
