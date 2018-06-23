1bd47353c12da963ca849a2ee151072c68cf49fb
Pushpendra kumar <pushpendra.kumar@tekmindz.com>
2018-06-23 12:36:36 +0530

    [Day Commit] : Update for mail service

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/serviceImpl/EmailServicesImpl.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/serviceImpl/EmailServicesImpl.java
@@ -1,61 +1,40 @@
 package com.vfoexchange.restServer.serviceImpl;
 
+import com.vfoexchange.restServer.model.Mail;
 import com.vfoexchange.restServer.service.EmailServices;
-import com.vfoexchange.restServer.util.AppUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
-import java.util.Properties;
+import java.util.Date;
 
 @Service("emailServices")
 public class EmailServicesImpl implements EmailServices {
 
     @Autowired
-    private Environment environment;
+    private JavaMailSender emailSender;
 
-
-    private static Logger LOGGER = LoggerFactory.getLogger(EmailServicesImpl.class);
-
+    private static Logger log = LoggerFactory.getLogger(EmailServicesImpl.class);
     @Override
-    public void sendMail(final String toEmailId) throws Exception {
+    public void sendMail(final Mail mail) {
+        log.info("Mail - Sending  Email for verify ");
+        MimeMessage msg = emailSender.createMimeMessage();
 
-        LOGGER.info("Mail - Sending  Email for verify ");
+        try{
+            msg.setSubject(mail.getSubject());
+            msg.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(mail.getTo(), false));
+            msg.setContent(mail.getContent(), "text/html; charset=utf-8");
+            msg.setSentDate(new Date());
+            emailSender.send(msg);
 
-        Properties props = System.getProperties();
-        props.put("mail.transport.protocol", environment.getProperty("mail.transport.protocol"));
-        props.put("mail.smtp.port", environment.getProperty("mail.smtp.port"));
-        props.put("mail.smtp.starttls.enable", environment.getProperty("mail.smtp.starttls.enable"));
-        props.put("mail.smtp.auth", environment.getProperty("mail.smtp.auth"));
 
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage msg = new MimeMessage(session);
-        Transport transport = null;
-        try {
-
-            msg.setFrom(new InternetAddress(environment.getProperty("mail.smtp.from"), environment.getProperty("mail.smtp.fromname")));
-            msg.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmailId));
-            msg.setSubject("VFOX Verify Email");
-            msg.setContent(AppUtil.getMailBody(AppUtil.getURL(AppUtil.getEncodedString(toEmailId))), "text/html");
-
-            transport = session.getTransport();
-            transport.connect(environment.getProperty("mail.smtp.host"), environment.getProperty("mail.smtp.username"), environment.getProperty("mail.smtp.password"));
-            transport.sendMessage(msg, msg.getAllRecipients());
-            LOGGER.info("Mail has been sent for verify ");
-        } catch (UnsupportedEncodingException | MessagingException ex) {
-            LOGGER.error("Error in sending mail " + ex.getMessage());
-
-        } finally {
-             transport.close();
+        }catch(MessagingException  ex ){
+            System.out.print("Error in sending mail "+ex.getMessage());
         }
 
     }
