9171385a42be69704f9941df2a6ce1a6d8c94d97
Pushpendra kumar <pushpendra.kumar@tekmindz.com>
2018-06-23 12:36:13 +0530

    [Day Commit] : Update for mail service

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/service/EmailServices.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/service/EmailServices.java
@@ -1,5 +1,7 @@
 package com.vfoexchange.restServer.service;
 
+import com.vfoexchange.restServer.model.Mail;
+
 /*
 Send an email through Amazon SES using the AWS SDK for Java
  */
@@ -8,5 +10,5 @@ public interface EmailServices {
     /*
     Method for send email
      */
-    public void sendMail(String toEmailId) throws Exception;
+    public void sendMail(Mail mail) throws Exception;
 }
