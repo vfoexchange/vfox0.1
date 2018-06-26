be2e2406e39dc86191488c0d3984338d28e27625
Pushpendra kumar <pushpendra.kumar@tekmindz.com>
2018-06-26 12:44:28 +0530

    Change after review

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/serviceImpl/EmailServicesImpl.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/serviceImpl/EmailServicesImpl.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import javax.mail.Message;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -26,42 +26,14 @@ public class EmailServicesImpl implements EmailServices {
         log.info("Mail - Sending  Email for verify ");
         MimeMessage msg = emailSender.createMimeMessage();
 
-        try{
+        try {
             msg.setSubject(mail.getSubject());
             msg.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(mail.getTo(), false));
             msg.setContent(mail.getContent(), "text/html; charset=utf-8");
             msg.setSentDate(new Date());
             emailSender.send(msg);
-        }catch(MessagingException  ex ){
-            System.out.print("Error in sending mail "+ex.getMessage());
-    @Override
-    public void sendMail(final String toEmailId) throws Exception {
-
-        LOGGER.info("Mail - Sending  Email for verify ");
-
-        Properties props = System.getProperties();
-        props.put("mail.transport.protocol", environment.getProperty("mail.transport.protocol"));
-        props.put("mail.smtp.port", environment.getProperty("mail.smtp.port"));
-        props.put("mail.smtp.starttls.enable", environment.getProperty("mail.smtp.starttls.enable"));
-        props.put("mail.smtp.auth", environment.getProperty("mail.smtp.auth"));
-
-        Session session = Session.getDefaultInstance(props);
-        MimeMessage msg = new MimeMessage(session);
-        Transport transport = null;
-        try {
-            msg.setFrom(new InternetAddress(environment.getProperty("mail.smtp.from"), environment.getProperty("mail.smtp.fromname")));
-            msg.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmailId));
-            msg.setSubject("VFOX Verify Email");
-            msg.setContent(AppUtil.getMailBody(AppUtil.getURL(AppUtil.getEncodedString(toEmailId))), "text/html");
-
-            transport = session.getTransport();
-            transport.connect(environment.getProperty("mail.smtp.host"), environment.getProperty("mail.smtp.username"), environment.getProperty("mail.smtp.password"));
-            transport.sendMessage(msg, msg.getAllRecipients());
-            LOGGER.info("Mail has been sent for verify ");
-        } catch (UnsupportedEncodingException | MessagingException ex) {
-            LOGGER.error("Error in sending mail " + ex.getMessage());
-        } finally {
-            transport.close();
+        } catch (MessagingException ex) {
+            System.out.print("Error in sending mail " + ex.getMessage());
         }
     }
 }
