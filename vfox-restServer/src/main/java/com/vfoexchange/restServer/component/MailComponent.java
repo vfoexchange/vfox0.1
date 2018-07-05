40a001e11fb9a695e66d74f2b76eb98c213abc33
Pushpendra kumar <pushpendra.kumar@tekmindz.com>
2018-07-05 11:37:25 +0530

    [Day Commit]:Update after review

--- vfox-restServer/src/main/java/com/vfoexchange/restServer/component/MailComponent.java
+++ vfox-restServer/src/main/java/com/vfoexchange/restServer/component/MailComponent.java
@@ -8,18 +8,25 @@ public class MailComponent {
 
     @Value("${spring.mail.host}")
     private String hostName;
+
     @Value("${spring.mail.port}")
     private String portNumber;
+
     @Value("${spring.mail.protocol}")
     private String protocolName;
+
     @Value("${spring.mail.username}")
     private String username;
+
     @Value("${spring.mail.password}")
     private String password;
+
     @Value("${spring.mail.properties.mail.smtp.auth}")
     private String auth;
+
     @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
     private String starttlsEnable;
+
     @Value("${spring.mail.smtp.socketFactory.class}")
     private String socketFactoryClass;
 
