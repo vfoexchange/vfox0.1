80c9d26419f16d8ff9618392a4a8404347afdb34
Pradeep kumar <pradeep.kumar@tekmindz.com>
2018-07-06 17:16:39 +0530

    [Day Commit] vfox-web - Add model popup on contactus page

---
+++ vfox-web/src/app/views/dashboard/maintain-website/select-provider/select-provider.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MaintainWebsite } from '../../../../services/maintain-website.service';
+import { ToastrService } from 'ngx-toastr';
+import { TreeviewItem, TreeviewConfig } from 'ngx-treeview';
+
+
+@Component({
+  selector: 'Tree-Structure',
+  templateUrl: './select-provider.component.html'
+})
+export class SelectProviderComponent {
+
+ 
+  dropdownEnabled = true;
+  items: TreeviewItem[];
+  values: number[];
+  config = TreeviewConfig.create({
+    hasAllCheckBox: false,
+    hasFilter: false,
+    hasCollapseExpand: false,
+    decoupleChildFromParent: false,
+    maxHeight: 400
+  });
+
+ 
+  constructor(private maintainWebsite: MaintainWebsite) {
+  }
+
+
+  ngOnInit() {
+    this.items = this.maintainWebsite.getBooks();
+  }
+
+  onFilterChange(value: string) {
+    console.log('filter:', value);
+  }
+
+}
+
